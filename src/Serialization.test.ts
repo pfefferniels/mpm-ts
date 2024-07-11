fa6b4101d456826a3f861f4c3988ac4b67f5ec06
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2024-07-11 10:39:22 +0200

    add performance name

--- src/Serialization.test.ts
+++ src/Serialization.test.ts
@@ -4,6 +4,8 @@ import { exportMPM, handleNode, MPM } from '.';
 test('serializes MPM', () => {
     const mpm = new MPM()
 
+    mpm.setPerformanceName('test performance')
+
     mpm.setMetadata({
         type: 'metadata',
         authors: [
@@ -38,5 +40,5 @@ test('serializes MPM', () => {
     const serialized = handleNode(mpm.doc)
     console.log(serialized)
 
-    expect(exportMPM(mpm)).toEqual("<mpm><metadata><author number=\"1\" #text=\"John Doe\"></author><author number=\"2\" #text=\"Jane Doe\"></author></metadata><performance name=\"\" pulsesPerQuarter=\"720\"><part><global><header></header><dated><articulation relativeDuration=\"0.5\" date=\"720\" xml:id=\"any_id\"></articulation></dated></global><part midi.port=\"0\" midi.channel=\"0\" number=\"1\"><header></header><dated><articulation relativeDuration=\"0.2\" date=\"1440\" xml:id=\"any_id\"></articulation></dated></part></part></performance></mpm>")
+    expect(exportMPM(mpm)).toEqual("<mpm><metadata><author number=\"1\" #text=\"John Doe\"></author><author number=\"2\" #text=\"Jane Doe\"></author></metadata><performance name=\"test performance\" pulsesPerQuarter=\"720\"><part><global><header></header><dated><articulation relativeDuration=\"0.5\" date=\"720\" xml:id=\"any_id\"></articulation></dated></global><part midi.port=\"0\" midi.channel=\"0\" number=\"1\"><header></header><dated><articulation relativeDuration=\"0.2\" date=\"1440\" xml:id=\"any_id\"></articulation></dated></part></part></performance></mpm>")
 });
