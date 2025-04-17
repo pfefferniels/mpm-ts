f84620cb1a787ac6ccf364656f8d2c58a301a0bf
Niels Pfeffer <47777840+pfefferniels@users.noreply.github.com>
2025-04-17 11:30:41 +0200

    add XML import

---
+++ src/Deserialization.ts
@@ -0,0 +1,114 @@
+import { XMLParser } from "fast-xml-parser";
+import { MPM } from ".";
+
+export const importMPM = (xml: string): MPM => {
+    const parser = new XMLParser({
+        preserveOrder: true,
+        ignoreAttributes: false,
+        attributeNamePrefix: "@_",
+        cdataPropName: "__cdata"
+    });
+
+    const parsed = parser.parse(xml) as any;
+
+    const rootNode = parsed.find((node: any) => typeof node === 'object');
+    if (!rootNode) throw new Error("Invalid XML: no root element");
+
+    const mpm = new MPM();
+    mpm.doc = parseNode(rootNode)
+    return mpm;
+}
+
+function parseNode(node: any): any {
+    const tag = Object.keys(node).find(k => k !== ':@');
+    const attrs = node[':@'] || {}
+
+    for (const [key, value] of Object.entries(attrs)) {
+        if (key.startsWith('@_')) {
+            attrs[key.slice(2)] = value;
+            delete attrs[key];
+        }
+    }
+
+    if (tag === 'mpm') {
+        return {
+            type: 'mpm',
+            performance: parseNode(node[tag].find(obj => 'performance' in obj)),
+            metadata: parseNode(node[tag].find(obj => 'metadata' in obj)),
+        }
+    }
+    else if (tag === 'performance') {
+        return {
+            type: 'performance',
+            name: attrs.name,
+            parts: new Map(
+                node[tag]
+                    .filter(obj => ('part' in obj) || ('global' in obj))
+                    .map(obj => parseNode(obj))
+            ),
+        }
+    }
+    else if (tag === 'part' || tag === 'global') {
+        return [
+            tag === 'global' ? 'global' : attrs.number,
+            {
+                type: 'part',
+                ...attrs,
+                header: parseNode(node[tag].find(obj => 'header' in obj)),
+                dated: parseNode(node[tag].find(obj => 'dated' in obj)),
+            }
+        ]
+    }
+    else if (tag === 'dated') {
+        const result = {
+            type: tag,
+            ...attrs
+        }
+
+        for (const datedNode of node[tag]) {
+            const datedTag = Object.keys(datedNode).find(k => k !== ':@');
+            if (datedTag) {
+                result[datedTag] = datedNode[datedTag].map(parseNode);
+            }
+        }
+
+        return result;
+    }
+    else if (tag === 'header') {
+        const result = {
+            type: tag,
+            ...attrs
+        }
+
+        for (const styleNode of node[tag]) {
+            const styleTag = Object.keys(styleNode).find(k => k !== ':@');
+            if (styleTag
+                && Array.isArray(styleNode[styleTag])
+                && styleNode[styleTag].length > 0
+            ) {
+                result[styleTag] = parseNode(styleNode[styleTag][0]);
+            }
+        }
+
+        return result;
+    }
+    else if (tag === 'styleDef') {
+        return {
+            type: 'styleDef',
+            ...attrs,
+            defs: node[tag].map(obj => parseNode(obj))
+        }
+    }
+
+
+    return {
+        'type': tag,
+        ...attrs,
+        children:
+            Array.isArray(node[tag])
+                ? node[tag].map(obj => {
+                    return parseNode(obj)
+                })
+                : []
+    }
+}
--- src/Serialization.test.ts
+++ src/Serialization.test.ts
@@ -1,5 +1,40 @@
 import { expect, test } from '@jest/globals';
 import { exportMPM, MPM } from '.';
+import { importMPM } from './Deserialization';
+
+const serialized = `
+<mpm>
+  <metadata>
+    <author number="1">John Doe</author>
+    <comment>based on musical intuition</comment>
+    <relatedResources uri="test.mei"></relatedResources>
+  </metadata>
+  <performance name="test performance" pulsesPerQuarter="720">
+    <global>
+      <header>
+        <articulationStyles>
+          <styleDef name="performance_style">
+            <articulationDef name="def_abc" relativeVelocity="-10"></articulationDef>
+          </styleDef>
+        </articulationStyles>
+      </header>
+      <dated>
+        <articulationMap>
+          <style xml:id="style_1" date="0" name.ref="performance_style"></style>
+          <articulation relativeDuration="0.5" date="720" xml:id="any_id"></articulation>
+        </articulationMap>
+      </dated>
+    </global>
+    <part midi.port="0" midi.channel="0" number="1" name="part_0">
+      <header></header>
+      <dated>
+        <articulationMap>
+          <articulation relativeDuration="0.2" date="1440" xml:id="any_id"></articulation>
+        </articulationMap>
+      </dated>
+    </part>
+  </performance>
+</mpm>`
 
 test('serializes MPM', () => {
   const mpm = new MPM()
@@ -49,37 +84,13 @@ test('serializes MPM', () => {
     relativeVelocity: -10
   }, 'global')
 
-  expect(exportMPM(mpm)).toEqual(`
-<mpm>
-  <metadata>
-    <author number="1">John Doe</author>
-    <comment>based on musical intuition</comment>
-    <relatedResources uri="test.mei"></relatedResources>
-  </metadata>
-  <performance name="test performance" pulsesPerQuarter="720">
-    <global>
-      <header>
-        <articulationStyles>
-          <styleDef name="performance_style">
-            <articulationDef name="def_abc" relativeVelocity="-10"></articulationDef>
-          </styleDef>
-        </articulationStyles>
-      </header>
-      <dated>
-        <articulationMap>
-          <style xml:id="style_1" date="0" name.ref="performance_style"></style>
-          <articulation relativeDuration="0.5" date="720" xml:id="any_id"></articulation>
-        </articulationMap>
-      </dated>
-    </global>
-    <part midi.port="0" midi.channel="0" number="1" name="part_0">
-      <header></header>
-      <dated>
-        <articulationMap>
-          <articulation relativeDuration="0.2" date="1440" xml:id="any_id"></articulation>
-        </articulationMap>
-      </dated>
-    </part>
-  </performance>
-</mpm>`)
+  expect(exportMPM(mpm)).toEqual(serialized)
 });
+
+test('deserializes MPM', () => {
+  const mpm = importMPM(serialized)
+
+  expect(mpm.getInstructions().length).toBe(2)
+  expect(mpm.getDefinitions('articulationDef').length).toBe(1)
+  expect(mpm.getStyles('articulation', 'global').length).toBe(1)
+})
